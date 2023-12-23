c422e029f03bd8c68605864e155b2180db34f420
yanaylax <yanaylax@gmail.com>
2023-12-23 20:27:06 +0200

    animation fix

--- src/hooks/useNba.ts
+++ src/hooks/useNba.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "../redux/store";
 import {
